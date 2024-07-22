8b2c3ac7e42ec9df7028be9de658dba45f457c45
Guilherme Bergue <bergueeg@gmail.com>
2024-07-22 20:43:18 -0300

    feat: layout reponsiveness

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,6 @@ import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
 import { email, github, linkedin } from "../../data/personal.json";
 
-
 export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
@@ -13,21 +12,32 @@ export const Contact = () => {
         <p>Feel free to reach out!</p>
       </div>
       <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a target="_blank" href={`mailto:${email}`}>{email}</a>
-        </li>
-        <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="LinkedIn icon"
-          />
-          <a target="_blank" href={linkedin}>{linkedin}</a>
-        </li>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a target="_blank" href={github}>{github}</a>
-        </li>
+        <div className={styles.linksContainer}>
+          <li className={styles.link}>
+            <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+            <a target="_blank" href={`mailto:${email}`}>
+              Email
+            </a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageUrl("contact/linkedinIcon.png")}
+              alt="LinkedIn icon"
+            />
+            <a target="_blank" href={linkedin}>
+              Linkedin
+            </a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageUrl("contact/githubIcon.png")}
+              alt="Github icon"
+            />
+            <a target="_blank" href={github}>
+              GitHub
+            </a>
+          </li>
+        </div>
       </ul>
     </footer>
   );
