e8d0350a12441c2f79a87ee1f3ff9a45f146df41
Guilherme Bergue <bergueeg@gmail.com>
2025-03-29 15:43:51 -0300

    update info

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,12 +12,17 @@ export const Hero = () => {
           <h1 className={styles.title}>Hi, I'm Guilherme</h1>
         </div>
         <p className={styles.description}>
-          Software Engineer with 3 years of hands-on experience.
+          Software Engineer with 4 years of hands-on experience.
           <p>Reach out if you'd like to learn more!</p>
         </p>
-        <a target="_blank" href="mailto:bergueeg@gmail.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div>
+          <a target="_blank" href="mailto:bergueeg@gmail.com" className={styles.contactBtn}>
+            Contact Me
+          </a>
+          <a target="_blank" href="https://calendly.com/bergueeg/30min" className={styles.contactBtn}>
+            Schedule a time
+          </a>
+        </div>
       </div>
       <img
         src={getImageUrl("hero/myPhoto.png")}
