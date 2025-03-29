e5a4c9e50d3fe62eda071e53e8917da891b2b978
Guilherme Bergue <bergueeg@gmail.com>
2025-03-29 15:25:18 -0300

    Add firebase analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import "./index.css";
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
+import "./firebase/init.ts"
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
